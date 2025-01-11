3c6e5ce5bb21011f80355029804bfe567fa4be61
Acengen <kocic999@yahoo.com>
2025-01-11 13:27:16 +0100

    user service and controller added

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
+import { UsersController } from './users.controller';
+import { UserService } from './users.service';
 
 @Module({
-    controllers: [],
-    providers: [],
+    controllers: [UsersController],
+    providers: [UserService],
 })
 
 export class UsersModule {}
